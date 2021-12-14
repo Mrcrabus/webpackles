abb58c7fc766a3efceefc5ed2f8ffb1346f7d064
Mrcrabus <bodenchyck97@mail.ru>
2021-12-14 20:19:48 +0300

    Well done

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import * as Request from './requests'
     try {
         // let auth = await Request.auth('a07797c659bd4c2b200d2f48c270140b', '12345')
         //     .then((res) => window.console.log(res))
-        let all = await Request.all().then((res) => window.console.log(res));
-        let add = await Request.add('Hello', 'world').then((res) => window.console.log(res))
+        await Request.all().then((res) => window.console.log(res));
+        await Request.add('Hello', 'world').then((res) => window.console.log(res))
 
     } catch (e) {
         window.console.log('error!');
